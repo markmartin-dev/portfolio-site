6c9f500798ad881116f37166777a633f4cd5a925
Mark Martin <development@roentgenmartin.com>
2026-02-27 19:33:20 -0500

    remove the gtag code and replace it with a plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [
+    react(), 
+    cloudflare(), 
+    VitePluginRadar({
+      analytics: {
+        id: "G-ZB7EHL8EEM",
+      },
+    })
+  ],
 })
